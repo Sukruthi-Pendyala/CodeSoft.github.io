513eb43672906c8a264deb6c1b24edb1ecaf41e0
Sukruthi-Pendyala <sukruthipendyala@gmail.com>
2025-03-25 15:32:56 +0530

    Add files via upload

---
+++ lc or uc .py
@@ -0,0 +1,10 @@
+ch=input("enter a character:")
+if(ch>='0' and ch<='9'):
+     print("digit")
+     elif(ch>='A' and ch<='Z'):
+                print("upper case character")
+                    elif(ch>='a' and ch<='z'):
+                        print("lower case character")
+                            else:
+                                print("special character")
+ 
